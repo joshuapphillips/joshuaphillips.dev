3c46a7125215d79fd26b613f79e4a0090c24335d
= <joshua.p.phillips@live.co.uk>
2023-11-26 14:14:51 +0000

    C moved

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,5 @@
         @include('partials/index/info')
         <livewire:contact-form />
     </x-layouts.container>
-    <x-miscellaneous.copyright class="text-center mb-5 lg:mb-0 lg:absolute lg:bottom-20 lg:-right-7G lg:-rotate-90" />
+    <x-miscellaneous.copyright class="text-center mb-5 lg:mb-0 lg:absolute lg:bottom-20 lg:-right-7 lg:-rotate-90" />
 </x-layouts.html>
